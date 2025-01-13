258b337746e746003c80b0cf7b27d2dbea74e86e
mostafa-muharam <mostafa.muharam@alfamedicalgroup.com>
2025-01-13 10:01:32 +0200

    added capacitor plugin to prject

---
+++ svelt-testing/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'alfamedicalgroup.com',
+  appName: 'svelt-testing',
+  webDir: 'dist'
+};
+
+export default config;
